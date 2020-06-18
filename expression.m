5a46010ffc9f529bda6da38e79965fd45075f80c
coderpankajp <61205130+coderpankajp@users.noreply.github.com>
2020-06-18 18:39:01 +0530

    Add files via upload

---
+++ expression.m
@@ -0,0 +1,8 @@
+%code for calculating displacement,velocity and acceleration.
+x = 4;
+d = x^3+2*x^2-x+9; %displacement
+v = 3*x^2+4*x-1; %velocity
+a = 6*x+4; %acceleration
+disp(d)
+disp(v)
+disp(a)
